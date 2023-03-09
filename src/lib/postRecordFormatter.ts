d9986e6529601e16c37a0cad0c0548260309a44f
spuithori <biz@spuit.tech>
2023-03-09 14:13:47 +0900

    fix.

--- src/lib/postRecordFormatter.ts
+++ src/lib/postRecordFormatter.ts
@@ -11,23 +11,21 @@ export function postRecordFormatter(record: object) {
 
     let cursor = 0;
     for (const entity of record.entities) {
-        if (entity.type === 'link') {
-            const typed = text.slice(entity.index.start, entity.index.end);
-            let plain = text.slice(cursor, entity.index.end);
-            plain = plain.replace(typed, '');
+        const typed = text.slice(entity.index.start, entity.index.end);
+        let plain = text.slice(cursor, entity.index.end);
+        plain = plain.replace(typed, '');
 
-            const plainObj = {
-                type: 'text',
-                content: plain,
-            }
-            const typedObj = {
-                type: entity.type,
-                content: typed,
-                url: entity.value,
-            };
-
-            textArray.push(plainObj, typedObj)
+        const plainObj = {
+            type: 'text',
+            content: plain,
         }
+        const typedObj = {
+            type: entity.type,
+            content: typed,
+            url: entity.value,
+        };
+
+        textArray.push(plainObj, typedObj)
 
         cursor = entity.index.end;
     }
