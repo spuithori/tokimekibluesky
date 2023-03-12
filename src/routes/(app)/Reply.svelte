1e530aebb4f464708c3675c95140fab504b68542
spuithori <biz@spuit.tech>
2023-03-12 17:44:56 +0900

    fix.

--- src/routes/(app)/Reply.svelte
+++ src/routes/(app)/Reply.svelte
@@ -38,17 +38,25 @@
 <div class="reply">
   <div class="publish-form">
     <textarea type="text" class="publish-form__input" disabled={isTextareaEnabled} bind:value={publishContent} on:keydown={publishKeypress} placeholder="Alt + Enter"></textarea>
-    <button class="publish-form__submit" on:click={publish}><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024">
-      <title></title>
-      <g id="icomoon-ignore">
-      </g>
-      <path fill="var(--primary-color)" d="M1009.376 5.12c-5.312-3.424-11.36-5.12-17.376-5.12-6.176 0-12.384 1.76-17.76 5.376l-960 640c-9.888 6.56-15.328 18.112-14.048 29.952 1.216 11.808 8.896 22.016 19.936 26.368l250.368 100.192 117.728 206.016c5.632 9.888 16.096 16 27.424 16.128 0.128 0 0.224 0 0.352 0 11.232 0 21.664-5.952 27.424-15.552l66.464-110.816 310.24 124.064c3.808 1.536 7.808 2.272 11.872 2.272 5.44 0 10.816-1.376 15.68-4.128 8.448-4.736 14.24-13.056 15.872-22.624l160-960c2.080-12.576-3.488-25.184-14.176-32.128zM100.352 664.864l741.6-494.432-539.2 577.184c-2.848-1.696-5.376-3.936-8.512-5.184l-193.888-77.568zM326.048 770.112c-0.064-0.128-0.16-0.192-0.224-0.32l606.176-648.8-516.768 805.184-89.184-156.064zM806.944 947.488l-273.312-109.312c-6.496-2.56-13.248-3.424-19.936-3.808l420.864-652.416-127.616 765.536z"></path>
-    </svg></button>
+    <button class="publish-form__submit" on:click={publish}><svg xmlns="http://www.w3.org/2000/svg" width="17" height="12.75" viewBox="0 0 17 12.75">
+      <path id="send" d="M0,0,17,6.375,0,12.75ZM0,5.1V7.65L8.5,6.375Z" fill="var(--bg-color-1)"/>
+    </svg>
+      送信</button>
   </div>
 </div>
 
-<style>
+<style lang="postcss">
   .reply {
       margin-top: 10px;
+  }
+
+  .publish-form__input {
+      padding-right: 100px;
+  }
+
+  .publish-form__submit {
+      position: absolute;
+      top: 10px;
+      right: 10px;
   }
 </style>
