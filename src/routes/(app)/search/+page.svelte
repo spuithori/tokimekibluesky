632d4b1c96b3096739abeb08784fc1a6a6f0546f
spuithori <biz@spuit.tech>
2023-03-21 12:43:25 +0900

    search previous step.

---
+++ src/routes/(app)/search/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+    const searchParams = $page.url.searchParams;
+    const query = searchParams.get('q');
+</script>
+
+<div>
+
+</div>
+
+<style>
+
+</style>
