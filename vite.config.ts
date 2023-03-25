ddeab485f969cf8ee9452d9bd00e5b3f2dd7b656
spuithori <biz@spuit.tech>
2023-03-25 13:08:44 +0900

    preparation support pwa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,70 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			workbox: {
+				mode: 'production',
+				runtimeCaching: [
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'gstatic-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							},
+						}
+					}
+				]
+			},
+			manifest: {
+				name: 'TOKIMEKI Bluesky',
+				short_name: 'TOKIMEKI',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				description: 'Bluesky web app',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			devOptions: {
+				enabled: true,
+				type: 'module',
+				navigateFallback: '/',
+			},
+		}),
+	]
 });
