31a06cf897ed157d2c3503a5dfd87f7f107530fd
spuithori <biz@spuit.tech>
2023-04-04 13:01:17 +0900

    support quote notification.

--- src/routes/(app)/Notification.svelte
+++ src/routes/(app)/Notification.svelte
@@ -17,7 +17,7 @@
   {:then list}
     {#each list as item}
       {#if (item.reason === 'like')}
-        <article class="notification-item">
+        <article class="notifications-item">
           <h2 class="notifications-item__title"><span class="notifications-item__name"><a  href="/profile/{item.author.handle}">{item.author.displayName || item.author.handle}</a></span> {$_('liked_your_post')}</h2>
 
           {#await $agent.getFeed(item.record.subject.uri)}
@@ -26,7 +26,7 @@
           {/await}
         </article>
       {:else if (item.reason === 'repost')}
-        <article class="notification-item">
+        <article class="notifications-item">
           <h2 class="notifications-item__title"><span class="notifications-item__name"><a href="/profile/{item.author.handle}">{item.author.displayName || item.author.handle}</a></span> {$_('reposted_your_post')}</h2>
 
           {#await $agent.getFeed(item.record.subject.uri)}
@@ -34,14 +34,25 @@
             <p class="notifications-item__content">{feed.post.record.text}</p>
           {/await}
         </article>
+      {:else if (item.reason === 'quote')}
+        <article class="notifications-item">
+          <h2 class="notifications-item__title"><span class="notifications-item__name"><a href="/profile/{item.author.handle}">{item.author.displayName || item.author.handle}</a></span> {$_('quoted_your_post')}</h2>
+
+          <p class="notifications-item__content">{item.record.text}</p>
+
+          {#await $agent.getFeed(item.reasonSubject)}
+          {:then feed}
+            <p class="notifications-item__quote">{feed.post.record.text}</p>
+          {/await}
+        </article>
       {:else if (item.reason === 'reply')}
-        <article class="notification-item">
+        <article class="notifications-item">
           <h2 class="notifications-item__title"><span class="notifications-item__name"><a href="/profile/{item.author.handle}">{item.author.displayName || item.author.handle}</a></span> {$_('replied_your_post')}</h2>
 
           <p class="notifications-item__content">{item.record.text}</p>
         </article>
       {:else if (item.reason === 'follow')}
-        <article class="notification-item notification-item--follow">
+        <article class="notifications-item notifications-item--follow">
           <h2 class="notifications-item__title"><span class="notifications-item__name"><a href="/profile/{item.author.handle}">{item.author.displayName || item.author.handle}</a></span> {$_('followed_you')}</h2>
           <UserFollowButton following="{item.author.viewer?.following}" user={item.author}></UserFollowButton>
         </article>
@@ -51,29 +62,32 @@
   {/await}
 </div>
 
-<style>
-  .notification-item {
+<style lang="postcss">
+  .notifications-item {
       border-bottom: 1px solid var(--border-color-1);
       padding: 10px 0;
-  }
 
-  .notification-item--follow {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-  }
+      &__title {
+          font-size: 15px;
+          line-height: 1.5;
+          font-weight: 600;
+      }
 
-  .notifications-item__title {
-      font-size: 15px !important;
-      line-height: 1.5 !important;
-      font-weight: 600;
-  }
+      &__content {
+          font-size: 14px;
+      }
 
-  .notification-item__name {
-    word-break: break-all;
-  }
+      &__quote {
+          border: 1px solid var(--border-color-1);
+          padding: 10px;
+          font-size: 13px;
+          margin-top: 10px;
+      }
 
-  .notifications-item__content {
-      font-size: 14px !important;
+      &--follow {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+      }
   }
 </style>
