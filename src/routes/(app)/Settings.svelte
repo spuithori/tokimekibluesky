7af6abfe86a47b587e3c5a6234b8fe9caac7381a
spuithori <biz@spuit.tech>
2023-04-15 00:31:06 +0900

    add about modal.

--- src/routes/(app)/Settings.svelte
+++ src/routes/(app)/Settings.svelte
@@ -4,12 +4,14 @@
     import { goto } from '$app/navigation';
     import { fade, fly } from 'svelte/transition';
     import AccountSwitcher from './AccountSwitcher.svelte';
+    import About from "./About.svelte";
 
     let darkModeToggle = JSON.parse(localStorage.getItem('darkmode')) === true;
     let nonotoToggle = JSON.parse(localStorage.getItem('nonoto')) === true;
     let themePick: string = localStorage.getItem('theme') || 'lightblue';
     let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
     let isAccountSwitcherOpen = false;
+    let isAboutOpen = false;
     const currentAccount = Number(localStorage.getItem('currentAccount') || '0' );
 
     $: {
@@ -45,10 +47,34 @@
         accounts = accounts;
         isAccountSwitcherOpen = isAccountSwitcherOpen !== true;
     }
+
+    function aboutToggle() {
+        isAboutOpen = isAboutOpen !== true;
+    }
 </script>
 
 <div>
-  <p class="setting-docs"><a href="https://tokimekibluesky-docs.vercel.app/" target="_blank" rel="noopener">{$_('document')}</a></p>
+  <div class="settings-row">
+    <p class="setting-docs"><a href="https://tokimekibluesky-docs.vercel.app/" target="_blank" rel="noopener">{$_('document')}</a></p>
+
+    <button class="settings-about" aria-label="About" on:click={aboutToggle}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+      <g id="グループ_85" data-name="グループ 85" transform="translate(-1270 -935)">
+        <circle id="楕円形_17" data-name="楕円形 17" cx="11" cy="11" r="11" transform="translate(1270 935)" fill="#f1f1f1"/>
+        <path id="パス_16" data-name="パス 16" d="M6.212-2.813H8.22c-.233-2.163,3.159-2.427,3.159-4.715,0-2.07-1.649-3.112-3.906-3.112a5.177,5.177,0,0,0-4.015,1.9L4.734-7.559a3.3,3.3,0,0,1,2.49-1.229c1.214,0,1.961.529,1.961,1.463C9.184-5.816,5.885-5.256,6.212-2.813ZM7.224,1.326A1.315,1.315,0,0,0,8.593-.043,1.319,1.319,0,0,0,7.224-1.428,1.328,1.328,0,0,0,5.854-.043,1.315,1.315,0,0,0,7.224,1.326Z" transform="translate(1273.697 950.772)" fill="#1d1d1d"/>
+      </g>
+    </svg>
+    </button>
+
+    {#if (isAboutOpen)}
+      <div class="about-box" transition:fly="{{ y: 30, duration: 250 }}">
+        <button class="about-box__close" on:click={aboutToggle}></button>
+
+        <div class="about-box__content">
+          <About></About>
+        </div>
+      </div>
+    {/if}
+  </div>
 
   <dl class="settings-group settings-group--column">
     <dt class="settings-group__name">
@@ -381,7 +407,53 @@
     }
 
     .setting-docs {
+    }
+
+    .settings-row {
         border-bottom: 1px solid var(--border-color-1);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         padding-bottom: 10px;
+    }
+
+    .about-box {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, .6);
+        z-index: 5000;
+        display: grid;
+        place-items: center;
+
+        &__content {
+            width: calc(100% - 20px);
+            height: max-content;
+            max-height: 90svh;
+            max-width: 740px;
+            overflow: auto;
+            overscroll-behavior-y: none;
+            background-color: var(--bg-color-1);
+            border-radius: 6px;
+            padding: 30px;
+            position: relative;
+        }
+
+        &__buttons {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        &__close {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+        }
     }
 </style>
