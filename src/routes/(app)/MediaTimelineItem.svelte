9d2a5deeacbb2fb5fb251ee3bac1e61e70fd6947
spuithori <biz@spuit.tech>
2023-04-14 22:30:40 +0900

    fix.

--- src/routes/(app)/MediaTimelineItem.svelte
+++ src/routes/(app)/MediaTimelineItem.svelte
@@ -28,6 +28,7 @@
   {#if (isOpen)}
     <div class="media-content-wrap" transition:fade="{{ duration: 300 }}">
       <button on:click={modalToggle} class="media-content-close gclose gbtn" aria-label="Close" data-taborder="3"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><g><g><path d="M505.943,6.058c-8.077-8.077-21.172-8.077-29.249,0L6.058,476.693c-8.077,8.077-8.077,21.172,0,29.249C10.096,509.982,15.39,512,20.683,512c5.293,0,10.586-2.019,14.625-6.059L505.943,35.306C514.019,27.23,514.019,14.135,505.943,6.058z"></path></g></g><g><g><path d="M505.942,476.694L35.306,6.059c-8.076-8.077-21.172-8.077-29.248,0c-8.077,8.076-8.077,21.171,0,29.248l470.636,470.636c4.038,4.039,9.332,6.058,14.625,6.058c5.293,0,10.587-2.019,14.624-6.057C514.018,497.866,514.018,484.771,505.942,476.694z"></path></g></g></svg></button>
+      <button on:click={modalToggle} class="media-content-close-bg"></button>
 
       <div class="media-content" transition:scale="{{duration: 350, opacity: 0.5, start: 0.8}}">
         <div class="media-content__image">
@@ -156,6 +157,14 @@
       }
   }
 
+  .media-content-close-bg {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      top: 0;
+      right: 0;
+  }
+
   .media-content {
       display: grid;
       grid-template-columns: auto 320px;
@@ -165,6 +174,7 @@
       border-radius: 10px;
       overflow: auto;
       overscroll-behavior: contain;
+      position: relative;
 
       @media (max-width: 959px) {
           display: block;
