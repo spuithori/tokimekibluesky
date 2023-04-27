a7bcc80a39899505e01d315a086cc1aac67ad8f7
spuithori <biz@spuit.tech>
2023-04-27 19:17:43 +0900

    modal close available page back.

--- src/routes/(app)/MediaTimelineItem.svelte
+++ src/routes/(app)/MediaTimelineItem.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-    import { fade, scale } from 'svelte/transition';
-    import TimelineItem from "./TimelineItem.svelte";
-    import {agent} from "$lib/stores";
-    import {AppBskyFeedDefs} from "@atproto/api";
-    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-    import '@splidejs/svelte-splide/css';
-    import { onDestroy } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import { agent } from '$lib/stores';
+    import { AppBskyFeedDefs } from '@atproto/api';
+    import MediaTimelineItemModal from './MediaTimelineItemModal.svelte';
+    import { goto } from '$app/navigation';
 
     export let data;
     let isOpen = false;
@@ -17,11 +15,15 @@
     function modalToggle() {
         isOpen = isOpen !== true;
         document.body.classList.toggle('scroll-lock', isOpen);
+
+        if (isOpen) {
+            goto('#open');
+        }
     }
 
-    onDestroy(async () => {
-        document.body.classList.remove('scroll-lock');
-    })
+    function handleClose() {
+        isOpen = false;
+    }
 </script>
 
 <div class="media-item" class:media-item--repost={isReasonRepost(data.reason)}
@@ -53,50 +55,12 @@
 
 {#if (isOpen)}
   <div class="media-content-wrap" transition:fade="{{ duration: 300 }}">
-    <button on:click={modalToggle} class="media-content-close gclose gbtn" aria-label="Close" data-taborder="3"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><g><g><path d="M505.943,6.058c-8.077-8.077-21.172-8.077-29.249,0L6.058,476.693c-8.077,8.077-8.077,21.172,0,29.249C10.096,509.982,15.39,512,20.683,512c5.293,0,10.586-2.019,14.625-6.059L505.943,35.306C514.019,27.23,514.019,14.135,505.943,6.058z"></path></g></g><g><g><path d="M505.942,476.694L35.306,6.059c-8.076-8.077-21.172-8.077-29.248,0c-8.077,8.076-8.077,21.171,0,29.248l470.636,470.636c4.038,4.039,9.332,6.058,14.625,6.058c5.293,0,10.587-2.019,14.624-6.057C514.018,497.866,514.018,484.771,505.942,476.694z"></path></g></g></svg></button>
-    <button on:click={modalToggle} class="media-content-close-bg"></button>
-
-    <div class="media-content" transition:scale="{{duration: 350, opacity: 0.5, start: 0.8}}">
-      <div class="media-content__image">
-
-
-        {#if (data.post.embed.images.length > 1)}
-
-          <Splide options={ {
-              gap   : '20px',
-          } }>
-            {#each data.post.embed.images as image}
-              <SplideSlide>
-                <img src="{image.fullsize}" alt="">
-              </SplideSlide>
-            {/each}
-          </Splide>
-        {:else}
-          {#each data.post.embed.images as image}
-            <img src="{image.fullsize}" alt="">
-          {/each}
-        {/if}
-      </div>
-
-      <div class="media-content__content">
-        <TimelineItem data={data} isMedia={true}></TimelineItem>
-      </div>
-    </div>
+    <MediaTimelineItemModal data={data} on:close={handleClose}></MediaTimelineItemModal>
   </div>
 {/if}
 
 <style lang="postcss">
-  .media-list {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 2px;
-
-      @media (max-width: 767px) {
-          grid-template-columns: repeat(2, 1fr);
-      }
-  }
-
-  .media-item {
+    .media-item {
       width: 100%;
       height: 100%;
       aspect-ratio: 1 / 1;
@@ -199,73 +163,6 @@
 
       @media (max-width: 767px) {
           padding: 10px;
-      }
-  }
-
-  .media-content-close {
-      position: fixed;
-      right: 20px;
-      top: 15px;
-      z-index: 50;
-      background-color: rgba(0, 0, 0, .32);
-      opacity: .7;
-      width: 35px;
-      height: 35px;
-
-      svg {
-          width: 18px;
-          height: auto;
-          display: block;
-          fill: #fff;
-      }
-  }
-
-  .media-content-close-bg {
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      top: 0;
-      right: 0;
-  }
-
-  .media-content {
-      display: grid;
-      grid-template-columns: auto 320px;
-      background-color: var(--bg-color-1);
-      padding: 40px;
-      gap: 40px;
-      border-radius: 10px;
-      overflow: auto;
-      overscroll-behavior: contain;
-      position: relative;
-
-      @media (max-width: 959px) {
-          display: block;
-          padding: 10px;
-          border-radius: 4px;
-      }
-
-      &__image {
-          height: 100%;
-
-          @media (max-width: 959px) {
-              height: auto;
-          }
-      }
-
-      &__content {
-
-      }
-
-      img {
-          width: 100%;
-          height: 100%;
-          max-height: 80vh;
-          object-fit: contain;
-
-          @media (max-width: 959px) {
-              height: auto;
-          }
       }
   }
 </style>
