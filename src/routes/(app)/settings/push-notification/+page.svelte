96b130b0b3240fc38e4bc9376153d0650689054c
spuithori <biz@spuit.tech>
2023-05-13 02:31:08 +0900

    ui renewal.

--- src/routes/(app)/settings/push-notification/+page.svelte
+++ src/routes/(app)/settings/push-notification/+page.svelte
@@ -37,7 +37,7 @@
 <div>
   <div class="settings-heading">
     <a href="/settings" class="settings-back"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16.97" viewBox="0 0 20 16.97">
-      <path id="arrow-left" d="M3.828,9,9.9,2.929,8.485,1.515,0,10l.707.707,7.778,7.778L9.9,17.071,3.828,11H20V9Z" transform="translate(0 -1.515)"/>
+      <path id="arrow-left" d="M3.828,9,9.9,2.929,8.485,1.515,0,10l.707.707,7.778,7.778L9.9,17.071,3.828,11H20V9Z" transform="translate(0 -1.515)" fill="var(--text-color-1)"/>
     </svg></a>
 
     <h1 class="settings-title">{$_('settings_push_notification')}</h1>
