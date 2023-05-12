781f3d01eeb251774a932394c1aa299358659782
spuithori <biz@spuit.tech>
2023-05-13 02:15:06 +0900

    ui renewal.

--- src/routes/(app)/settings/mutes/+page.svelte
+++ src/routes/(app)/settings/mutes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+    import {_} from 'svelte-i18n';
+    import { settings } from '$lib/stores';
 
+    $: {
+
+    }
 </script>
 
 <div>
-  MUTES
+  <div class="settings-heading">
+    <a href="/settings" class="settings-back"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="16.97" viewBox="0 0 20 16.97">
+      <path id="arrow-left" d="M3.828,9,9.9,2.929,8.485,1.515,0,10l.707.707,7.778,7.778L9.9,17.071,3.828,11H20V9Z" transform="translate(0 -1.515)"/>
+    </svg></a>
+
+    <h1 class="settings-title">{$_('settings_mutes')}</h1>
+  </div>
+
+  <div class="settings-wrap">
+    工事中<br>
+    Under construction
+  </div>
 </div>
