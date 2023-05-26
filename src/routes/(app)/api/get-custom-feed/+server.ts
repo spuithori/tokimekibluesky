3b10ab76749017447144cf6725cb04e4df7f3f90
spuithori <biz@spuit.tech>
2023-05-26 12:23:31 +0900

    improvement custom feeds. add ga4 experimental.

--- src/routes/(app)/api/get-custom-feed/+server.ts
+++ src/routes/(app)/api/get-custom-feed/+server.ts
@@ -5,10 +5,17 @@ export async function POST ({ request }) {
         const json = await request.json();
         const feed = json.feed;
         const cursor = json.cursor;
-        const res = await fetch(CUSTOM_FEED_API + feed + '&limit=20&cursor=' + cursor , {
+        const host = feed.split(':')[3]?.split('/')[0];
+        const uri = 'https://' + host + '/xrpc/app.bsky.feed.getFeedSkeleton?feed=';
+        const id = feed.split('/').slice(-1)[0];
+        const aturi = 'at://did:web:' + host + '/app.bsky.feed.generator/';
+        const cursorOp = cursor ? '&cursor=' + cursor : '';
+
+        const res = await fetch(uri + aturi + encodeURIComponent(id) + '&limit=20' + cursorOp , {
             method: 'GET',
         });
+        const resjson = await res.json();
 
-        return new Response(JSON.stringify(await res.json()), { status: 200 });
+        return new Response(JSON.stringify(resjson), { status: 200 });
     }
 }
