5c9c90e228354a37fa139643162e4fa01a18097a
spuithori <biz@spuit.tech>
2023-06-17 00:34:06 +0900

    custom feeds other servers.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -246,18 +246,16 @@
 							</li>
 						{/if}
 
-						{#if ($agent.agent.service.host === 'bsky.social')}
-							{#each customFeeds as feed}
-								<li class="algo-nav-list__item">
-									<button class="algo-nav-button" on:click={() => {openAlgoNav({type: 'custom', algorithm: feed.uri, name: feed.name})}} >{feed.name}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-										<g id="グループ_89" data-name="グループ 89" transform="translate(-1059 -638)">
-											<rect id="長方形_76" data-name="長方形 76" width="16" height="16" transform="translate(1059 638)" fill="var(--danger-color)"/>
-											<path id="パス_23" data-name="パス 23" d="M-4.216-12.446v-.781c0-1.385-1.208-2.063-2.859-2.063a4.99,4.99,0,0,0-5.157,5.3A4.893,4.893,0,0,0-7.251-4.961c1.4,0,3.006-.589,3.006-1.975v-.751h-.074a3.975,3.975,0,0,1-2.667.973c-1.9,0-3.212-1.194-3.212-3.566A3.065,3.065,0,0,1-7.06-13.537a3.617,3.617,0,0,1,2.77,1.09Z" transform="translate(1075.028 655.929)" fill="#fff"/>
-										</g>
-									</svg></button>
-								</li>
-							{/each}
-						{/if}
+						{#each customFeeds as feed}
+							<li class="algo-nav-list__item">
+								<button class="algo-nav-button" on:click={() => {openAlgoNav({type: 'custom', algorithm: feed.uri, name: feed.name})}} >{feed.name}<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+									<g id="グループ_89" data-name="グループ 89" transform="translate(-1059 -638)">
+										<rect id="長方形_76" data-name="長方形 76" width="16" height="16" transform="translate(1059 638)" fill="var(--danger-color)"/>
+										<path id="パス_23" data-name="パス 23" d="M-4.216-12.446v-.781c0-1.385-1.208-2.063-2.859-2.063a4.99,4.99,0,0,0-5.157,5.3A4.893,4.893,0,0,0-7.251-4.961c1.4,0,3.006-.589,3.006-1.975v-.751h-.074a3.975,3.975,0,0,1-2.667.973c-1.9,0-3.212-1.194-3.212-3.566A3.065,3.065,0,0,1-7.06-13.537a3.617,3.617,0,0,1,2.77,1.09Z" transform="translate(1075.028 655.929)" fill="#fff"/>
+									</g>
+								</svg></button>
+							</li>
+						{/each}
 
 						{#each $userLists as list}
 							{#if (list.owner === $agent.did())}
