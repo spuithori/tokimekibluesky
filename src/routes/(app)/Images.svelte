3a3008489e4c600a697febfa253a6353df6bd9ac
spuithori <biz@spuit.tech>
2023-06-24 21:49:19 +0900

    add data saver mode.

--- src/routes/(app)/Images.svelte
+++ src/routes/(app)/Images.svelte
@@ -5,7 +5,7 @@
     export let images: any[];
 
     let galleryImages = [];
-    let isFold = $settings?.design.postsImageLayout === 'folding';
+    let isFold = $settings?.design.postsImageLayout === 'folding' || $isDataSaving;
 
     for (const image of images) {
         galleryImages.push({
@@ -27,7 +27,7 @@
         isFold = false;
     }
 </script>
-{#if isFold || $isDataSaving}
+{#if isFold}
   <button class="image-unfold-button" on:click={unfold}>
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17.143" viewBox="0 0 20 17.143">
       <path id="image" d="M49.143,64H34.857A2.86,2.86,0,0,0,32,66.857V78.286a2.86,2.86,0,0,0,2.857,2.857H49.143A2.86,2.86,0,0,0,52,78.286V66.857A2.86,2.86,0,0,0,49.143,64Zm-3.571,2.857A2.143,2.143,0,1,1,43.429,69a2.143,2.143,0,0,1,2.143-2.143ZM34.857,79.714a1.429,1.429,0,0,1-1.429-1.429V75.267L37.662,71.5a2.146,2.146,0,0,1,2.938.085l2.9,2.893-5.233,5.233Zm15.714-1.429a1.429,1.429,0,0,1-1.429,1.429H40.287l5.421-5.421a2.13,2.13,0,0,1,2.752-.007l2.112,1.76Z" transform="translate(-32 -64)" fill="#aeaeae"/>
