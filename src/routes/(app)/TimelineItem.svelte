30705b45b086afb50e8ba791ecf8d13df9e8b4ff
spuithori <biz@spuit.tech>
2023-09-21 11:51:04 +0900

    welcome back to uri copy menu.

--- src/routes/(app)/TimelineItem.svelte
+++ src/routes/(app)/TimelineItem.svelte
@@ -409,12 +409,12 @@
           </button>
         </li>
 
-        <!-- <li class="timeline-menu-list__item timeline-menu-list__item--copy-url">
+        <li class="timeline-menu-list__item timeline-menu-list__item--copy-url">
           <button class="timeline-menu-list__button" on:click={copyThreadUrl}>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
             {$_('copy_url')}
           </button>
-        </li> -->
+        </li>
 
         <li class="timeline-menu-list__item timeline-menu-list__item--copy-handle">
           <button class="timeline-menu-list__button" on:click={copyHandle}>
