8243fbc04b34bb1534e77f0a00bec6b27380f6b4
spuithori <biz@spuit.tech>
2023-10-25 20:46:09 +0900

    fix.

--- src/routes/(app)/mutes/[id]/+page.svelte
+++ src/routes/(app)/mutes/[id]/+page.svelte
@@ -41,7 +41,7 @@
       <h1 class="column-heading__title">{$_('mutes_list')} ({_agent?.agent.session.handle || 'error'})</h1>
 
       <div class="column-heading__buttons column-heading__buttons--right">
-        <a class="settings-back" href="/static">
+        <a class="settings-back" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
         </a>
       </div>
