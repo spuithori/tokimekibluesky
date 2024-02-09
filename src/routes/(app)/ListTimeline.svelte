ed323e9eef0acf80da637de736101b5a33f1eb2f
spuithori <biz@spuit.tech>
2024-02-09 12:41:23 +0900

    local list fix.

--- src/routes/(app)/ListTimeline.svelte
+++ src/routes/(app)/ListTimeline.svelte
@@ -44,11 +44,10 @@
         feed = feedPool.slice(0, 20);
         feedPool = feedPool.slice(20);
 
+        await poolRecalc(feedPool);
+        column.data.feed = [...column.data.feed, ...feed];
+
         if (cursors.some(item => item.cursor !== undefined)) {
-            await poolRecalc(feedPool);
-
-            column.data.feed = [...column.data.feed, ...feed];
-
             loaded();
         } else {
             complete();
