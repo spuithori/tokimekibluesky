1d4ead445d6a223ff508d4829b29ec02066b4c3d
spuithori <biz@spuit.tech>
2024-03-06 17:02:59 +0900

    fix media timeline alt text.

--- src/routes/(app)/MediaTimelineItemModal.svelte
+++ src/routes/(app)/MediaTimelineItemModal.svelte
@@ -64,13 +64,13 @@
           } }>
           {#each data.post.embed.images as image}
             <SplideSlide>
-              <img src="{image.fullsize}" alt="" width={image?.aspectRatio?.width} height={image?.aspectRatio?.height}>
+              <img src="{image.fullsize}" alt="{image.alt}" width={image?.aspectRatio?.width} height={image?.aspectRatio?.height}>
             </SplideSlide>
           {/each}
         </Splide>
       {:else}
         {#each data.post.embed.images as image}
-          <img src="{image.fullsize}" alt="" width={image?.aspectRatio?.width} height={image?.aspectRatio?.height}>
+          <img src="{image.fullsize}" alt="{image.alt}" width={image?.aspectRatio?.width} height={image?.aspectRatio?.height}>
         {/each}
       {/if}
     </div>
