21edc1ce24bb8957ecf738721c6b4d9fb8ef2030
spuithori <biz@spuit.tech>
2024-04-10 02:15:14 +0900

    fix.

--- src/lib/components/post/TimelineWarn.svelte
+++ src/lib/components/post/TimelineWarn.svelte
@@ -28,7 +28,7 @@
     <p class="timeline-warn-title"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--danger-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-alert-triangle"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"/><path d="M12 9v4"/><path d="M12 17h.01"/></svg></p>
     <ul class="timeline-warn-list">
       <li>
-        {#if (labels[0].source.type === 'user' || labels[0].label.src === 'did:plc:ar7c4by46qjdydhdevvrndac' || !labels[0]?.labelDef?.locales[0]?.name)}
+        {#if (labels[0]?.source?.type === 'user' || labels[0]?.label?.src === 'did:plc:ar7c4by46qjdydhdevvrndac' || !labels[0]?.labelDef?.locales[0]?.name)}
           {$_('labeling_' + labels[0].label?.val)}
         {:else}
           {labels[0]?.labelDef?.locales[0]?.name}
@@ -56,7 +56,7 @@
       <p class="timeline-warn-title"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--danger-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-alert-triangle"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"/><path d="M12 9v4"/><path d="M12 17h.01"/></svg></p>
       <ul class="timeline-warn-list">
         <li>
-          {#if (labels[0].source.type === 'user' || labels[0].label.src === 'did:plc:ar7c4by46qjdydhdevvrndac' || !labels[0]?.labelDef?.locales[0]?.name)}
+          {#if (labels[0]?.source?.type === 'user' || labels[0]?.label?.src === 'did:plc:ar7c4by46qjdydhdevvrndac' || !labels[0]?.labelDef?.locales[0]?.name)}
             {$_('labeling_' + labels[0].label?.val)}
           {:else}
             {labels[0]?.labelDef?.locales[0]?.name}
