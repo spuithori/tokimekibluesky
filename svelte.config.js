230703930c90d57f80c9b89624cd9e22907a5b54
spuithori <biz@spuit.tech>
2024-04-18 13:21:13 +0900

    change package.

--- svelte.config.js
+++ svelte.config.js
@@ -18,16 +18,16 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    serviceWorker: {
+      register: false,
+    },
     files: {
       serviceWorker: 'src/sw.ts',
     },
-    serviceWorker: {
-      register: false,
-    }
   },
 
   plugins: {
-    
+
   },
 };
 
