eb703ee61b044fb14703c38f482ace2d6d0f622b
spuithori <biz@spuit.tech>
2024-06-14 20:43:23 +0900

    fix.

--- src/lib/components/chat/ChatTiptap.svelte
+++ src/lib/components/chat/ChatTiptap.svelte
@@ -247,9 +247,9 @@
     <slot name="avatar"></slot>
 
     <div class="chat-editor" bind:this={element}>
-      <button class="chat-editor-stamp-button">
+      <!-- <button class="chat-editor-stamp-button">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-3)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-stamp"><path d="M5 22h14"/><path d="M19.27 13.73A2.5 2.5 0 0 0 17.5 13h-11A2.5 2.5 0 0 0 4 15.5V17a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-1.5c0-.66-.26-1.3-.73-1.77Z"/><path d="M14 13V8.5C14 7 15 7 15 5a3 3 0 0 0-3-3c-1.66 0-3 1-3 3s1 2 1 3.5V13"/></svg>
-      </button>
+      </button> -->
     </div>
 
     <button class="chat-editor-submit" disabled={isPublishEnabled} on:click={() => {dispatch('publish')}}>
