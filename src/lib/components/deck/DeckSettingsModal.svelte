121cb6d72b2b36c3af2b703c3db9c86a2b0636ac
spuithori <biz@spuit.tech>
2024-06-22 15:20:49 +0900

    add lang filter notice.

--- src/lib/components/deck/DeckSettingsModal.svelte
+++ src/lib/components/deck/DeckSettingsModal.svelte
@@ -581,6 +581,8 @@
 
                     {#if langFilterEnabled}
                         <div class="lang-filter-wrap">
+                            <p class="notice"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--primary-color)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-alert-triangle"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"/><path d="M12 9v4"/><path d="M12 17h.01"/></svg>{$_('lang_filter_notice')}</p>
+
                             <div class="lang-filter-list">
                                 {#each languageMap as [k, v]}
                                     <div class="lang-filter-list__item">
