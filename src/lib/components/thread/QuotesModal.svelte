9756660e0fba9c55c3ab1956a98a2fe6ae92c1e0
spuithori <biz@spuit.tech>
2024-08-29 17:17:26 +0900

    add quote count and quoted by view.

---
+++ src/lib/components/thread/QuotesModal.svelte
@@ -0,0 +1,48 @@
+<script>
+    import {_} from "svelte-i18n";
+    import {createEventDispatcher} from 'svelte';
+    import { agent } from "$lib/stores";
+    const dispatch = createEventDispatcher();
+    import InfiniteLoading from 'svelte-infinite-loading';
+    import Modal from "$lib/components/ui/Modal.svelte";
+    import TimelineItem from "../../../routes/(app)/TimelineItem.svelte";
+
+    export let uri;
+    let quotes = [];
+    let cursor;
+
+    async function handleLoadMore({ detail: { loaded, complete } }) {
+        try {
+            const res = await $agent.agent.api.app.bsky.feed.getQuotes({uri: uri, cursor: cursor});
+            cursor = res.data.cursor;
+            const wrapRes = res.data.posts.map(post => {
+                return {
+                    post: post,
+                }
+            });
+            quotes = [...quotes, ...wrapRes];
+
+            if (cursor) {
+                loaded();
+            } else {
+                complete();
+            }
+        } catch (e) {
+            console.error(e);
+            complete();
+        }
+    }
+</script>
+
+<Modal title="{$_('quote')}" on:close>
+  <div class="quotes">
+    {#each quotes as quote }
+      <TimelineItem data={quote}></TimelineItem>
+    {/each}
+  </div>
+
+  <InfiniteLoading on:infinite={handleLoadMore}>
+    <p slot="noMore" class="infinite-nomore">もうないよ</p>
+    <p slot="noResults"></p>
+  </InfiniteLoading>
+</Modal>
