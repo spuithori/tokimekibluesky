9c54687bec03cf9010029fa9c9956007d5450f3a
spuithori <biz@spuit.tech>
2024-11-02 20:04:36 +0900

    fix.

--- src/routes/(app)/profile/[handle]/+layout.svelte
+++ src/routes/(app)/profile/[handle]/+layout.svelte
@@ -174,7 +174,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
               </a>
             {:else if (isLabeler && profile?.did)}
-              <LabelerSubscribeButton did={profile.did}></LabelerSubscribeButton>
+              
             {:else}
               <div class="profile-follow-button profile-follow-button--me">
                 <UserEdit {profile} on:update={onProfileUpdate}></UserEdit>
@@ -193,7 +193,11 @@
       <div class="user-profile-wrap">
         {#if profile}
           <UserProfile handle={handle} profile={profile} isLabeler={isLabeler} on:refresh={handleRefresh}>
-            <UserFollowButton following="{profile.viewer?.following}" user={profile}></UserFollowButton>
+            {#if (profile.did !== $agent.did() && !isLabeler)}
+              <UserFollowButton following="{profile.viewer?.following}" user={profile}></UserFollowButton>
+            {:else if (isLabeler && profile?.did)}
+              <LabelerSubscribeButton did={profile.did}></LabelerSubscribeButton>
+            {/if}
           </UserProfile>
         {/if}
 
