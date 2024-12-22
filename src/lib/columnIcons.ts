5e1f2371902b93d1add0858d5ae5474324b6c1ab
spuithori <biz@spuit.tech>
2024-12-23 00:15:16 +0900

    fix.

--- src/lib/columnIcons.ts
+++ src/lib/columnIcons.ts
@@ -1,4 +1,4 @@
-import { Aperture, Bell, Bird, Bookmark, Bot, Box, Camera, Cat, Clapperboard, Clock3, Dog, FlaskConical, Heart, Home, Image, List, Newspaper, Pencil, Rainbow, Rss, Sparkles, Speaker, Star, Sun, Tent, TreePine, Apple, Beer, Coffee, IceCream, CakeSlice, Soup, UtensilsCrossed, Search, Repeat2, UserRound, Music, AtSign, Mic, Squirrel, PawPrint, Moon, Clover, Rocket, Gamepad2, Ghost, Crown, Gem, MessageCircleHeart, MessageCircleQuestion, Hash, Palette, Paintbrush, TypeOutline, NotepadText, Shirt, CandyCane, Snowflake, TreePalm, Cannabis, MapPin, Pin, Cherry, Earth, IceCreamCone, Nut, UserRoundSearch, UsersRound, ShoppingBasket, Castle, CassetteTape, Car, CarFront, Bike, Luggage, Sailboat, ThumbsUp, KeyRound, ChartSpline, PartyPopper, Cpu, Book, NotebookText, LibraryBig, BookOpenText, Bone, Fish, Candy } from 'lucide-svelte';
+import { Aperture, Bell, Bird, Bookmark, Bot, Box, Camera, Cat, Clapperboard, Clock3, Dog, FlaskConical, Heart, Home, Image, List, Newspaper, Pencil, Rainbow, Rss, Sparkles, Speaker, Star, Sun, Tent, TreePine, Apple, Beer, Coffee, IceCream, CakeSlice, Soup, UtensilsCrossed, Search, Repeat2, UserRound, Music, AtSign, Mic, Squirrel, PawPrint, Moon, Clover, Rocket, Gamepad2, Ghost, Crown, Gem, MessageCircleHeart, MessageCircleQuestion, Hash, Palette, Paintbrush, TypeOutline, NotepadText, Shirt, CandyCane, Snowflake, TreePalm, Cannabis, MapPin, Pin, Cherry, Earth, IceCreamBowl, Nut, UserRoundSearch, UsersRound, ShoppingBasket, Castle, CassetteTape, Car, CarFront, Bike, Luggage, Sailboat, ThumbsUp, KeyRound, ChartSpline, PartyPopper, Cpu, Book, NotebookText, LibraryBig, BookOpenText, Bone, Fish, Candy } from 'lucide-svelte';
 
 export const iconMap = new Map();
 
@@ -66,7 +66,7 @@ iconMap.set('home', Home)
     .set('pin', Pin)
     .set('cherry', Cherry)
     .set('earth', Earth)
-    .set('icecreamcone', IceCreamCone)
+    .set('icecreamcone', IceCreamBowl)
     .set('nut', Nut)
     .set('userroundsearch', UserRoundSearch)
     .set('usersround', UsersRound)
