b4db86dc9d95e461a577cd514e502326e2222719
spuithori <biz@spuit.tech>
2024-12-23 23:48:39 +0900

    add column add button in side columns menu.

--- src/lib/components/side/SideColumns.svelte
+++ src/lib/components/side/SideColumns.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import {_} from 'svelte-i18n';
+  import { isColumnModalOpen } from "$lib/stores";
   import ColumnList from "$lib/components/column/ColumnList.svelte";
   import {getColumnState} from "$lib/classes/columnState.svelte";
+  import {SquarePlus} from "lucide-svelte";
 
   let { onviewcolumn = () => {} } = $props();
   const columnState = getColumnState();
@@ -11,10 +14,29 @@
   {#if (columns.length)}
     <ColumnList items={columns} {onviewcolumn}></ColumnList>
   {/if}
+
+  <button class="column-add-button" onclick={() => {$isColumnModalOpen = true}}>
+    <SquarePlus color="var(--bg-color-1)" size="20"></SquarePlus>
+    {$_('feed_quick_add')}
+  </button>
 </div>
 
 <style lang="postcss">
   .side-columns {
       padding: 16px;
+  }
+
+  .column-add-button {
+      color: var(--bg-color-1);
+      background-color: var(--primary-color);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 4px;
+      height: 40px;
+      border-radius: var(--border-radius-3);
+      width: 100%;
+      margin-top: 16px;
+      font-weight: bold;
   }
 </style>
