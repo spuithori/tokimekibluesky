0974716168e69793dbaeb9821fb6ab3fc8f93629
spuithori <biz@spuit.tech>
2025-03-20 14:53:22 +0900

    optimize.

--- vite.config.ts
+++ vite.config.ts
@@ -18,38 +18,6 @@ export default defineConfig({
 			mode: 'production',
 			strategies: 'injectManifest',
 			filename: 'sw.ts',
-			workbox: {
-				runtimeCaching: [
-					{
-						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'google-fonts-cache',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 365
-							},
-							cacheableResponse: {
-								statuses: [0, 200]
-							}
-						}
-					},
-					{
-						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'gstatic-fonts-cache',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 365
-							},
-							cacheableResponse: {
-								statuses: [0, 200]
-							},
-						}
-					}
-				]
-			},
 			manifest: {
 				name: 'TOKIMEKI',
 				short_name: 'TOKIMEKI',
