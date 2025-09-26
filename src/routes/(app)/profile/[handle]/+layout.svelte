1a580f97a4f831d1210cf3da00f794d3f3d4a3de
spuithori <biz@spuit.tech>
2025-09-26 19:13:10 +0900

    fix.

--- src/routes/(app)/profile/[handle]/+layout.svelte
+++ src/routes/(app)/profile/[handle]/+layout.svelte
@@ -184,7 +184,7 @@
                 </button>
               {/if}
 
-              <a class="profile-heading-button" href="/search?q=from:{handle}%20">
+              <a class="profile-heading-button" href="/search?q=from:{profile?.handle}%20">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
               </a>
             {:else if (isLabeler && profile?.did)}
@@ -206,7 +206,7 @@
                 <StatusEditModal {_agent} onclose={() => {isStatusEditOpen = false; handleRefresh()}}></StatusEditModal>
               {/if}
 
-              <a class="profile-heading-button" href="/search?q=from:{handle}%20">
+              <a class="profile-heading-button" href="/search?q=from:{profile?.handle}%20">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--text-color-1)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
               </a>
             {/if}
