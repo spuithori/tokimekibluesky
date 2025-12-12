c79dece04628d87340a71b32752d488dd424f3a9
spuithori <biz@spuit.tech>
2025-12-12 19:22:55 +0900

    optimize.

--- src/routes/(app)/api/oauth/jwks.json/+server.ts
+++ src/routes/(app)/api/oauth/jwks.json/+server.ts
@@ -35,7 +35,7 @@ export async function GET() {
             status: 200,
             headers: {
                 'Content-Type': 'application/json',
-                'Cache-Control': 'public, max-age=3600',
+                'Cache-Control': 'public, max-age=86400, s-maxage=86400',
                 'Access-Control-Allow-Origin': '*',
             }
         });
