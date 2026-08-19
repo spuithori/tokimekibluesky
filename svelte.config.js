472431f97228b28ff57dc96295b961c5b415c4a8
spuithori <biz@spuit.tech>
2026-08-19 22:57:35 +0900

    try.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const adapter = process.env.CF_PAGES || process.env.WORKERS_CI
+  ? cloudflare
+  : vercel;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
